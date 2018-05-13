a39c324e623db9b1217d7f3c39e94461f3123f29
Sébastien Watteau <swatteau@gmail.com>
2018-05-13 09:56:19 +0200

    More cleanup

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,7 @@ use sdl2::keyboard::Keycode;
 use sdl2::render::{Canvas, TextureCreator};
 use sdl2::video::{Window, WindowContext};
 use sdl2::Sdl;
+use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
@@ -43,11 +44,11 @@ pub mod painter;
 pub mod shadow;
 pub mod tileset;
 
-use game::Level;
+use game::{Direction, Level};
 use painter::Painter;
 use tileset::Tileset;
 
-pub fn main() -> Result<(), Box<std::error::Error>> {
+pub fn main() -> Result<(), Box<Error>> {
     // Read command line arguments
     let yml = load_yaml!("clap.yml");
     let matches = App::from_yaml(yml).get_matches();
@@ -56,23 +57,19 @@ pub fn main() -> Result<(), Box<std::error::Error>> {
     let fullscreen = matches.is_present("fullscreen");
     let slc_file = matches.value_of("slc_file").unwrap();
 
-    let collection = load_slc_file(slc_file)?;
+    // Load the level collection file
+    let levels = load_slc_file(slc_file)?;
 
-    let sdl_context = sdl2::init()?;
+    // Initialize SDL components
+    let sdl = sdl2::init()?;
+    let _ = sdl2::image::init(INIT_PNG)?;
+    let ttf_context = sdl2::ttf::init()?;
 
-    let _ = sdl2::image::init(INIT_PNG).unwrap();
-    let ttf_context = sdl2::ttf::init().unwrap();
-
-    let window = get_window(&sdl_context, width, height, fullscreen);
-
-    let mut canvas = window
-        .into_canvas()
-        .build()
-        .unwrap_or_else(|err| panic!("Failed to get an SDL canvas for the main window: {}", err));
-
+    let window = create_window(&sdl, width, height, fullscreen)?;
+    let mut canvas = window.into_canvas().build()?;
     let texture_creator = canvas.texture_creator();
 
-    let painter = {
+    let mut painter = {
         let big_set = load_tileset(
             &texture_creator,
             "assets/image/tileset.png",
@@ -80,7 +77,7 @@ pub fn main() -> Result<(), Box<std::error::Error>> {
             171,
             83,
             40,
-        );
+        )?;
         let small_set = load_tileset(
             &texture_creator,
             "assets/image/tileset-small.png",
@@ -88,19 +85,94 @@ pub fn main() -> Result<(), Box<std::error::Error>> {
             85,
             41,
             20,
-        );
+        )?;
         Painter::new(&mut canvas, big_set, small_set, &ttf_context)
     };
 
-    mainloop(&sdl_context, &collection, painter, &mut canvas);
+    mainloop(&sdl, &levels, &mut painter, &mut canvas);
 
     Ok(())
 }
 
+/// Builds levels from a level collection file in the SLC format.
+fn load_slc_file<P: AsRef<Path>>(path: P) -> Result<Vec<Level>, error::SokobanError> {
+    let mut levels = Vec::new();
+
+    let parser = {
+        let file = File::open(path.as_ref())?;
+        EventReader::new(BufReader::new(file))
+    };
+
+    let mut level_title = String::new();
+    let mut level_data = String::new();
+    let mut reading_level = false;
+    for event in parser {
+        match event {
+            Ok(XmlEvent::StartElement {
+                ref name,
+                ref attributes,
+                ..
+            }) => {
+                if name.local_name == "L" {
+                    reading_level = true;
+                } else if name.local_name == "Level" {
+                    if let Some(id) = attributes.iter().find(|&attr| attr.name.local_name == "Id") {
+                        level_title = id.value.clone();
+                    }
+                }
+            }
+            Ok(XmlEvent::EndElement { name }) => {
+                if name.local_name == "Level" {
+                    let mut level = Level::from_str(&level_data)?;
+                    level.set_title(level_title.clone());
+                    levels.push(level);
+                    level_data.clear();
+                }
+            }
+            Ok(XmlEvent::Characters(ref data)) => {
+                if reading_level {
+                    level_data.push_str(data);
+                    level_data.push('\n');
+                }
+            }
+            _ => {}
+        }
+    }
+
+    Ok(levels)
+}
+
+/// Creates the SDL window
+fn create_window(sdl: &Sdl, width: u32, height: u32, fullscreen: bool) -> Result<Window, Box<Error>> {
+    let mut window_builder = sdl.video()?.window("sokoban-rs", width, height);
+    if fullscreen {
+        window_builder.fullscreen();
+    } else {
+        window_builder.position_centered();
+    }
+    let window = window_builder.opengl().build()?;
+    Ok(window)
+}
+
+/// Loads a tileset
+fn load_tileset<P: AsRef<Path>>(
+    texture_creator: &TextureCreator<WindowContext>,
+    path: P,
+    width: u32,
+    height: u32,
+    effective_height: u32,
+    offset: i32,
+) -> Result<Tileset, Box<Error>> {
+    let texture = texture_creator.load_texture(path.as_ref())?;
+    let tileset = Tileset::new(texture, width, height, effective_height, offset);
+    Ok(tileset)
+}
+
+/// Main game event loop
 fn mainloop(
-    sdl_context: &Sdl,
+    sdl: &Sdl,
     levels: &[Level],
-    mut painter: Painter,
+    painter: &mut Painter,
     canvas: &mut Canvas<Window>,
 ) {
     let mut collection = levels.into_iter();
@@ -108,7 +180,7 @@ fn mainloop(
     let mut level = reference_level.clone();
 
     let mut running = true;
-    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut event_pump = sdl.event_pump().unwrap();
     let mut skip = false;
     while running {
         if level.is_completed() || skip {
@@ -135,25 +207,25 @@ fn mainloop(
                 keycode: Some(Keycode::Left),
                 ..
             } => {
-                level.step(game::Direction::Left);
+                level.step(Direction::Left);
             }
             Event::KeyDown {
                 keycode: Some(Keycode::Right),
                 ..
             } => {
-                level.step(game::Direction::Right);
+                level.step(Direction::Right);
             }
             Event::KeyDown {
                 keycode: Some(Keycode::Up),
                 ..
             } => {
-                level.step(game::Direction::Up);
+                level.step(Direction::Up);
             }
             Event::KeyDown {
                 keycode: Some(Keycode::Down),
                 ..
             } => {
-                level.step(game::Direction::Down);
+                level.step(Direction::Down);
             }
             Event::KeyDown {
                 keycode: Some(Keycode::R),
@@ -170,83 +242,4 @@ fn mainloop(
             _ => {}
         }
     }
-}
-
-fn get_window(sdl_context: &Sdl, width: u32, height: u32, fullscreen: bool) -> Window {
-
-    let video_subsystem = sdl_context
-        .video()
-        .unwrap_or_else(|err| panic!("Failed to initialize the video subsystem: {}", err));
-
-    let mut window_builder = video_subsystem.window("sokoban-rs", width, height);
-    if fullscreen {
-        window_builder.fullscreen();
-    } else {
-        window_builder.position_centered();
-    }
-
-    let window = window_builder
-        .opengl()
-        .build()
-        .unwrap_or_else(|err| panic!("Failed to create the window: {}", err));
-
-    window
-}
-
-fn load_tileset<P: AsRef<Path>>(
-    texture_creator: &TextureCreator<WindowContext>,
-    path: P,
-    width: u32,
-    height: u32,
-    effective_height: u32,
-    offset: i32,
-) -> Tileset {
-    let texture = texture_creator.load_texture(path.as_ref()).unwrap();
-    Tileset::new(texture, width, height, effective_height, offset)
-}
-
-/// Builds levels from a level collection file in the SLC format.
-fn load_slc_file<P: AsRef<Path>>(path: P) -> Result<Vec<Level>, error::SokobanError> {
-    let mut collection = Vec::new();
-    let file = try!(File::open(path.as_ref()));
-    let reader = BufReader::new(file);
-    let parser = EventReader::new(reader);
-
-    let mut level_title = String::new();
-    let mut level_str = String::new();
-    let mut reading_level = false;
-    for e in parser {
-        match e {
-            Ok(XmlEvent::StartElement {
-                ref name,
-                ref attributes,
-                ..
-            }) => {
-                if name.local_name == "L" {
-                    reading_level = true;
-                } else if name.local_name == "Level" {
-                    if let Some(id) = attributes.iter().find(|&attr| attr.name.local_name == "Id") {
-                        level_title = id.value.clone();
-                    }
-                }
-            }
-            Ok(XmlEvent::EndElement { name }) => {
-                if name.local_name == "Level" {
-                    let mut level = try!(Level::from_str(&level_str));
-                    level.set_title(level_title.clone());
-                    collection.push(level);
-                    level_str.clear();
-                }
-            }
-            Ok(XmlEvent::Characters(ref data)) => {
-                if reading_level {
-                    level_str.push_str(data);
-                    level_str.push('\n');
-                }
-            }
-            _ => {}
-        }
-    }
-
-    Ok(collection)
 }
